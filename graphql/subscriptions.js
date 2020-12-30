480d5b7da40996dce133ad235c6ff54647e0a99d
Michael Liendo <mtliendo@gmail.com>
2020-12-30 13:54:10 -0600

    add stuff

---
+++ graphql/subscriptions.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateMessage = /* GraphQL */ `
+  subscription OnCreateMessage {
+    onCreateMessage {
+      id
+      text
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onUpdateMessage = /* GraphQL */ `
+  subscription OnUpdateMessage {
+    onUpdateMessage {
+      id
+      text
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeleteMessage = /* GraphQL */ `
+  subscription OnDeleteMessage {
+    onDeleteMessage {
+      id
+      text
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;

67decd018873519970cc40e1b5cfbe8904ee1030
Michael Liendo <mtliendo@gmail.com>
2020-12-30 13:49:49 -0600

    init setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,56 @@
+import React from 'react'
 import '../styles/globals.css'
-
+import Amplify from 'aws-amplify'
+import {
+  AmplifyAuthenticator,
+  AmplifySignOut,
+  AmplifySignUp,
+} from '@aws-amplify/ui-react'
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components'
+import config from '../aws-exports'
+Amplify.configure(config)
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [authState, setAuthState] = React.useState()
+  const [user, setUser] = React.useState(null)
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState)
+
+      if (nextAuthState === AuthState.SignedIn) {
+        const { email, sub, ...attributes } = authData.attributes
+
+        setUser({ email, username: sub, attributes })
+      }
+    })
+  }, [])
+
+  return authState === AuthState.SignedIn && user ? (
+    <main>
+      hello
+      <AmplifySignOut />
+      <Component {...pageProps} />
+    </main>
+  ) : (
+    <main>
+      <header>
+        <h1 style={{ textAlign: 'center' }}>Welcome to my app!</h1>
+      </header>
+      <main>
+        <AmplifyAuthenticator>
+          <AmplifySignUp
+            slot="sign-up"
+            formFields={[
+              { type: 'username' },
+              { type: 'email' },
+              { type: 'password' },
+            ]}
+          />
+        </AmplifyAuthenticator>
+      </main>
+      <footer>Hello I'm a footer</footer>
+    </main>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,57 @@
-import Head from 'next/head'
+import React from 'react'
 import styles from '../styles/Home.module.css'
+import { API } from 'aws-amplify'
+import { listMessages } from '../graphql/queries'
+import { createMessage } from '../graphql/mutations'
+import { onCreateMessage } from '../graphql/subscriptions'
 
 export default function Home() {
+  const [messageContent, setMessageContent] = React.useState('')
+  const [messages, setMessages] = React.useState([])
+
+  React.useEffect(() => {
+    API.graphql({ query: listMessages }).then((messageData) =>
+      setMessages(messageData.data.listMessages.items)
+    )
+  }, [])
+
+  React.useEffect(() => {
+    const subscription = API.graphql({ query: onCreateMessage }).subscribe({
+      next: ({ provider, value }) => {
+        const messageDetails = value.data.onCreateMessage
+        console.log(messages)
+        setMessages([...messages, messageDetails])
+      },
+    })
+
+    return () => subscription.unsubscribe()
+  }, [messages])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    API.graphql({
+      query: createMessage,
+      variables: { input: { text: messageContent } },
+    }).then((res) => setMessages([...messages, res.data.createMessage]))
+  }
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+      {messages.map((message) => (
+        <p>
+          {message.owner}: {message.text}
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter a message
+          <input
+            value={messageContent}
+            onChange={(e) => setMessageContent(e.target.value)}
+          />
+        </label>
+        <button>enter</button>
+      </form>
     </div>
   )
 }
